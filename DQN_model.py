38b832fa4788d94550a2cfb5a2d8d48b1a566abc
DingFong <a890702000@gmail.com>
2024-04-07 14:51:57 +0800

    submission

--- DQN_model.py
+++ DQN_model.py
@@ -3,11 +3,43 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 
+# class Q_net(nn.Module):
+#     def __init__(self, input_shape, n_actions):
+#         super(Q_net, self).__init__()
+#         self.conv = nn.Sequential(
+#             nn.Conv2d(input_shape[0], 32, kernel_size=8, stride = 4),
+#             nn.ReLU(),
+#             nn.Conv2d(32, 64, kernel_size=4, stride=2),
+#             nn.ReLU(),
+#             nn.Conv2d(64, 64, kernel_size=3, stride=1),
+#             nn.ReLU(),
+#         )
+#         conv_output_shape = self.get_conv_output_size(input_shape)
+
+#         self.fc = nn.Sequential(
+#             nn.Linear(conv_output_shape, 512),
+#             nn.ReLU(),
+#             nn.Linear(512, n_actions)
+#         )
+
+#     def get_conv_output_size(self, input_shape):
+#         input_ = torch.zeros((1, *input_shape))
+#         # print(np.prod(self.conv(input_).size()))
+#         return np.prod(self.conv(input_).size())
+    
+#     def forward(self, x):
+#         conv_out = self.conv(x).view(x.size()[0], -1)
+
+#         return self.fc(conv_out)
+    
+
 class Q_net(nn.Module):
     def __init__(self, input_shape, n_actions):
         super(Q_net, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride = 4),
+            nn.Conv2d(input_shape[0], 8, kernel_size=8, stride = 4),
+            nn.ReLU(),
+            nn.Conv2d(8, 32, kernel_size=4, stride=2),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
@@ -19,7 +51,9 @@ class Q_net(nn.Module):
         self.fc = nn.Sequential(
             nn.Linear(conv_output_shape, 512),
             nn.ReLU(),
-            nn.Linear(512, n_actions)
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, n_actions)
         )
 
     def get_conv_output_size(self, input_shape):
